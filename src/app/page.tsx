e07554789c5145d84e92f33c6f03714501af7f68
Alison G. Martins <odevalison@gmail.com>
2025-10-15 03:36:06 -0300

    feat: add create account link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,14 @@ const Authentication = () => {
     <div className="flex min-h-screen flex-col items-center justify-center gap-8 py-5">
       <section className="flex max-w-full min-w-2xl flex-col items-center gap-6">
         <AuthenticationForm />
-        <p>
-          <Link href="/account/create">Crie sua conta</Link>
+        <p className="text-brand-white flex items-center justify-center gap-1 text-sm">
+          Ainda não possui uma conta?
+          <Link
+            href="/account/create"
+            className="text-brand-red-900 font-semibold hover:underline"
+          >
+            Crie sua conta
+          </Link>
         </p>
       </section>
     </div>
