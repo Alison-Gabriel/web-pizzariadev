dd4aaa1a1c153ed21144b5240b9c15a4df9efae1
Alison G. Martins <odevalison@gmail.com>
2025-10-15 03:45:07 -0300

    fix: change form section width and container padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,15 @@ import { AuthenticationForm } from "./components/AuthenticationForm";
 
 const Authentication = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-8 py-5">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-8 px-5">
       <Image
         src="/logo-pizzaria.svg"
         height={90}
         width={335}
         alt="Logo Pizzaria"
       />
-      <section className="flex max-w-full min-w-2xl flex-col items-center gap-6">
+
+      <section className="flex w-2xl max-w-full flex-col items-center gap-6">
         <AuthenticationForm />
         <p className="text-brand-white flex items-center justify-center gap-1 text-sm">
           Ainda não possui uma conta?
