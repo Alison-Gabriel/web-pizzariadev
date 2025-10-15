c6b60449b2a23438b22a984c47caaf37437b9d82
Alison G. Martins <odevalison@gmail.com>
2025-10-15 03:42:15 -0300

    feat: add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 import { AuthenticationForm } from "./components/AuthenticationForm";
@@ -5,6 +6,12 @@ import { AuthenticationForm } from "./components/AuthenticationForm";
 const Authentication = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-8 py-5">
+      <Image
+        src="/logo-pizzaria.svg"
+        height={90}
+        width={335}
+        alt="Logo Pizzaria"
+      />
       <section className="flex max-w-full min-w-2xl flex-col items-center gap-6">
         <AuthenticationForm />
         <p className="text-brand-white flex items-center justify-center gap-1 text-sm">
