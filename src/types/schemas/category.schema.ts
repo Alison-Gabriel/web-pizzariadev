a50a3734073d98a9bfe6a78ec64bda8268b71ad2
Alison Gabriel <odevalison@gmail.com>
2025-10-19 01:35:21 -0300

    feat(schema): add schema to validate new categories

---
+++ src/types/schemas/category.schema.ts
@@ -0,0 +1,9 @@
+import z from "zod";
+
+const newCategorySchema = z.object({
+  name: z.string("O nome inserido é inválido.").min(1, "O nome é obrigatório."),
+});
+
+type NewCategorySchema = z.infer<typeof newCategorySchema>;
+
+export { type NewCategorySchema, newCategorySchema };
