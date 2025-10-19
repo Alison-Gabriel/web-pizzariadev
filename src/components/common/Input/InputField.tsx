23c5ec990583aa61f8951c90b3c723b2c2557c03
Alison Gabriel <odevalison@gmail.com>
2025-10-19 01:41:53 -0300

    fix(input): add text as default input type

--- src/components/common/Input/InputField.tsx
+++ src/components/common/Input/InputField.tsx
@@ -2,10 +2,11 @@ import { ComponentPropsWithRef } from "react";
 
 type InputFieldProps = ComponentPropsWithRef<"input">;
 
-const InputField = ({ ref, ...props }: InputFieldProps) => {
+const InputField = ({ ref, type = "text", ...props }: InputFieldProps) => {
   return (
     <input
       ref={ref}
+      type={type}
       className="border-brand-gray-100 bg-brand-dark-900 text-brand-white focus:border-brand-white w-full rounded-lg border px-2.5 py-1.5 shadow-md outline-none"
       {...props}
     />
