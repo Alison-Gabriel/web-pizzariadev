583b4337666f9562d32b6912a2d76f8c2b4f98de
Alison Gabriel <odevalison@gmail.com>
2025-10-24 01:57:48 -0300

    refactor(schema): change image to FileList instance and change schema name

--- src/types/schemas/product.ts
+++ src/types/schemas/product.ts
@@ -2,19 +2,17 @@ import z from "zod";
 
 export const imageAllowedTypes = ["image/jpeg", "image/png"];
 
-export const newProductSchema = z.object({
+export const productSchema = z.object({
   category: z.string().nonempty({ error: "A categoria e obrigatoria." }),
   name: z.string().nonempty({ error: "O nome e obrigatorio." }),
   price: z.string().nonempty({ error: "O preco e obrigatorio." }),
   description: z.string().nonempty({ error: "A descricao e obrigatoria." }),
   image: z
-    .instanceof(FileList)
-    .refine((files) => files.length, { error: "A imagem e obrigatoria" })
-    .refine(
-      (files) => files.length && imageAllowedTypes.includes(files[0].type),
-      { error: "Apenas imagens .jpeg e .png sao permitidas" },
-    )
-    .transform((files) => files[0] as File),
+    .instanceof(File, { error: "Envie um arquivo de imagem valido." })
+    .nonoptional({ error: "A imagem e obrigatoria." })
+    .refine((file) => file && imageAllowedTypes.includes(file.type), {
+      error: "Apenas arquivos de imagem PNG e JPEG sao permitidos.",
+    }),
 });
 
-export type NewProductSchema = z.infer<typeof newProductSchema>;
+export type ProductSchema = z.infer<typeof productSchema>;
