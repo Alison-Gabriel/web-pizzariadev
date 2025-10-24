40d0f4756ab233148b03f60a00aeaa972de29169
Alison Gabriel <odevalison@gmail.com>
2025-10-24 01:39:48 -0300

    fix(textarea): remove name property and set rows property as 8

--- src/app/dashboard/new-product/components/Textarea.tsx
+++ src/app/dashboard/new-product/components/Textarea.tsx
@@ -2,14 +2,12 @@ import { ComponentProps } from "react";
 
 type TextareaProps = ComponentProps<"textarea">;
 
-export const Textarea = ({ name, ...props }: TextareaProps) => {
+export const Textarea = ({ ...props }: TextareaProps) => {
   return (
     <textarea
       {...props}
-      name={name}
-      placeholder="Digite a descricao do produto"
       className="border-brand-gray-100 bg-brand-dark-900 text-brand-white focus:border-brand-white focus:outline-brand-gray-100/20 w-full resize-none rounded-lg border px-2.5 py-1.5 shadow-md focus:outline-2"
-      rows={5}
+      rows={8}
     ></textarea>
   );
 };
